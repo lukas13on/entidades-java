1a5a4d62889f3832f46e9cae9f393018f1138628
Lucas de Oliveira Neitzke <luka.pc.pc@gmail.com>
2022-09-08 17:18:13 -0300

    aaa

--- lista.sql
+++ lista.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE Lista02;
 
-use Lista02;
+USE Lista02;
 
 CREATE TABLE Alunos(
     codigo int,
@@ -9,7 +9,7 @@ CREATE TABLE Alunos(
     cidade varchar(50)
 );
 
-desc Alunos;
+DESC Alunos;
 
 CREATE TABLE Alunos2(
     codigo int,
@@ -19,7 +19,7 @@ CREATE TABLE Alunos2(
 );
 
 CREATE TABLE Funcionarios(
-    codigo int PRIMARY KEY NOT NULL,
+    codigo int primary key not null,
     nome varchar(255),
     telefone varchar(255),
     cidade varchar(255),
@@ -42,7 +42,7 @@ CREATE TABLE Fornecedores(
 );
 
 CREATE TABLE Livros(
-    codigo int PRIMARY KEY NOT NULL,
+    codigo int primary key not null,
     nome varchar(255),
     categoria varchar(255),
     resumo varchar(255),
@@ -50,14 +50,14 @@ CREATE TABLE Livros(
     precovenda float
 );
 
-desc Funcionarios;
+DESC Funcionarios;
 
-desc Fornecedores;
+DESC Fornecedores;
 
-desc Livros;
+DESC Livros;
 
 CREATE TABLE Estoque(
-    codigo int PRIMARY KEY NOT NULL,
+    codigo int primary key not null,
     nomedoproduto varchar(255),
     categoria varchar(255),
     quantidade int,
@@ -65,12 +65,131 @@ CREATE TABLE Estoque(
 );
 
 CREATE TABLE Notas(
-    codigo int PRIMARY KEY NOT NULL,
+    codigo int primary key not null,
     nomedoaluno varchar(255),
     bimestre int
 );
 
 CREATE TABLE Caixa(
-    codigo int PRIMARY KEY NOT NULL,
-    data_caixa date
+    codigo int primary key not null,
+    data_caixa datetime,
+    descricao varchar(255),
+    debito decimal(10, 2),
+    credito decimal(10, 2)
 );
+
+CREATE TABLE contasApagar(
+    codigo int primary key not null,
+    data_conta datetime,
+    descricao varchar(255),
+    valor decimal(10, 2),
+    data_pagamento datetime
+);
+
+CREATE TABLE contasAReceber(
+    codigo int primary key not null,
+    data_conta datetime,
+    descricao varchar(255),
+    valor decimal(10, 2),
+    data_recebimento datetime
+);
+
+CREATE TABLE Filmes(
+    codigo int primary key not null,
+    nome varchar(255),
+    cantor varchar(255),
+    ano int,
+    quantidadedemusicas int
+);
+
+DROP TABLE Alunos2;
+
+-- Comentado para o codigo executar
+-- DESC Alunos2;
+DROP TABLE Livros;
+
+DROP TABLE contasApagar;
+
+DROP TABLE contasAReceber;
+
+DROP TABLE Filmes;
+
+DESC Estoque;
+
+DESC Notas;
+
+DESC Caixa;
+
+ALTER TABLE
+    Alunos RENAME TO superAlunos;
+
+DESC superAlunos;
+
+ALTER TABLE
+    Estoque RENAME TO Produtos;
+
+ALTER TABLE
+    Notas RENAME TO Aprovados;
+
+ALTER TABLE
+    Aprovados RENAME TO Notas;
+
+-- DROP TABLE Dinheiro;
+-- A tabela (dinheiro) nao foi iniciada em nenhum momento na lista de exercicios
+DROP TABLE Notas;
+
+ALTER TABLE
+    superAlunos RENAME TO Alunos;
+
+ALTER TABLE
+    Alunos RENAME TO Estudantes;
+
+ALTER TABLE
+    Estudantes RENAME TO superEstudantes;
+
+DESC superEstudantes;
+
+DROP TABLE superEstudantes;
+
+-- Comentado para o codigo executar
+-- DESC superEstudantes;
+-- DESC Alunos;
+-- Tabela Alunos criada novamente para executar
+CREATE TABLE Alunos(
+    codigo int,
+    nome varchar(50),
+    telefone varchar(50),
+    cidade varchar(50)
+);
+
+ALTER TABLE
+    Alunos
+ADD
+    COLUMN estado varchar(255);
+
+-- Apaga a tabela Caixa para cria-la novamente
+DROP TABLE Caixa;
+
+CREATE TABLE Caixa(
+    codigo int primary key not null,
+    data_caixa datetime,
+    descricao varchar(255),
+    debito decimal(10, 2),
+    credito decimal(10, 2)
+);
+
+ALTER TABLE
+    Caixa
+ADD
+    COLUMN observacao varchar(255),
+ADD
+    COLUMN cpf varchar(255);
+
+DESC Caixa;
+
+ALTER TABLE
+    Caixa
+ADD
+    COLUMN saldo decimal(10, 2);
+
+-- Fim
